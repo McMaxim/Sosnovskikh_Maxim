9eb26d9d7b46844288402058774db0e4d1930e27
McMaxim <94379675+McMaxim@users.noreply.github.com>
2024-10-14 00:37:12 +0300

    Update BubbleSortStrategyTest.java

--- src/main/java/SortStartegies/BubbleSortStrategyTest.java
+++ src/main/java/SortStartegies/BubbleSortStrategyTest.java
@@ -1,6 +1,6 @@
 package SortStartegies;
 
-import org.junit.jupiter.api.Test;
+import org.testng.annotations.Test;
 import java.util.Arrays;
 import java.util.List;
 import static org.junit.jupiter.api.Assertions.*
